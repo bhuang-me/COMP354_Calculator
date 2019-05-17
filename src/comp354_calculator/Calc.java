dd85604d36d1b3f4baaa57700d63ca1ad860e148
Brandon Huang <brandonbgda@hotmail.com>
2019-05-17 18:09:26 -0400

    add Calc class with helper functions

---
+++ src/comp354_calculator/Calc.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp354_calculator;
+
+/** SUBJECT TO CHANGE */
+public class Calc {
+    
+    public static double exp (double arg1, double arg2) {
+        return 0.0;
+    } 
+    
+    public static double sin () {
+        return 0.0;
+    }
+    
+    public static double naturalExp () {
+        return 0.0;
+    }
+    
+    public static double decimalExp (double power) {
+        return exp(10, power);
+    }
+    
+    public static double sqrt() {
+        return 0.0;
+    }
+    
+    
+}
+
+
