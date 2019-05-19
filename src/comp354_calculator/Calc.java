09254dca884550cb0c397fd9cace9448c989e089
Brandon Huang <brandonbgda@hotmail.com>
2019-05-19 17:27:12 -0400

    add square root function

--- src/comp354_calculator/Calc.java
+++ src/comp354_calculator/Calc.java
@@ -24,8 +24,20 @@ public class Calc {
         return exp(10, power);
     }
     
-    public static double sqrt() {
-        return 0.0;
+    public static double sqrt(double arg) {
+        /* Calculates the square root of a number using the Babylonian method */
+        if(arg <= 0) {
+            return 0.0;
+        }
+        double guess = 0.0;
+        double res = 1.0;
+        while(res != guess) {
+            guess = res;
+            res = (res + arg / res) / 2;
+            System.out.println("Next guess is " + guess + ", result approximation is " + res);
+        }
+        
+        return res;
     }
     
     
