9d7e4593ac33824c05b69420f2d0fd861ac6c31b
Brandon Huang <brandonbgda@hotmail.com>
2019-05-22 01:46:13 -0400

    add comments and throw exception

--- src/comp354_calculator/Calc.java
+++ src/comp354_calculator/Calc.java
@@ -26,15 +26,20 @@ public class Calc {
     
     public static double sqrt(double arg) {
         /* Calculates the square root of a number using the Babylonian method */
-        if(arg <= 0) {
-            return 0.0;
+        if(arg < 0) {
+            throw new IllegalArgumentException("Cannot calculate negative square roots.");
+        } else if (arg == 0) {
+            return 0;
         }
         double guess = 0.0;
         double res = 1.0;
+        /* res and arg/res are overestimates and underestimates (or vice versa) for the root of arg
+        The closer the estimates are, the better the next guess will be.
+        We loop this algorithm until we converge to the true root
+        (or when the difference between the guess and result is lower than a certain amount) */
         while(res != guess) {
             guess = res;
             res = (res + arg / res) / 2;
-            System.out.println("Next guess is " + guess + ", result approximation is " + res);
         }
         
         return res;
